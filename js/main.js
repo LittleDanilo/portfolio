67e8f005ecad8a9556d5792687bb5b9e6d949973
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2024-08-15 11:31:31 -0300

    update frontend

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 function OnOffLight() {
     var body = document.querySelector("body");
-    var light = document.getElementsByClassName("beam");
     if (body.className == "night") {
-        body.classList.add("day");
-        body.classList.remove("night");
-        light.style.visibility = "hiden";
-
+        document.querySelector("#beam").style.visibility = "hidden";
+        setTimeout(() => {document.querySelector("#beam").style.visibility = "visible";}, 50);
+        setTimeout(() => {document.querySelector("#beam").style.visibility = "hidden";}, 150);
+        setTimeout(() =>{
+            body.classList.add("day");
+            body.classList.remove("night");
+        }, 320);
     } else {
-        body.classList.add("night");
-        body.classList.remove("day");
+        document.querySelector("#beam").style.visibility = "visible";
+        setTimeout(() => {document.querySelector("#beam").style.visibility = "hidden";}, 50);
+        setTimeout(() => {document.querySelector("#beam").style.visibility = "visible";}, 150);
+        setTimeout(() =>{
+            body.classList.add("night");
+            body.classList.remove("day");
+        }, 320);
     };
 };
