4739c066e495e8ae19eadfed6fc70bd79124e183
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2024-08-15 13:33:10 -0300

    update front-end: add footer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function OnOffLight() {
     var body = document.querySelector("body");
     if (body.className == "night") {
-        document.querySelector("#beam").style.visibility = "hidden";
-        setTimeout(() => {document.querySelector("#beam").style.visibility = "visible";}, 50);
-        setTimeout(() => {document.querySelector("#beam").style.visibility = "hidden";}, 150);
+        document.querySelector("#beam").style.opacity = "0%";
+        setTimeout(() => {document.querySelector("#beam").style.opacity = "45%"}, 50);
+        setTimeout(() => {document.querySelector("#beam").style.opacity = "0%"}, 150);
         setTimeout(() =>{
             body.classList.add("day");
             body.classList.remove("night");
         }, 320);
     } else {
-        document.querySelector("#beam").style.visibility = "visible";
-        setTimeout(() => {document.querySelector("#beam").style.visibility = "hidden";}, 50);
-        setTimeout(() => {document.querySelector("#beam").style.visibility = "visible";}, 150);
+        document.querySelector("#beam").style.opacity = "45%";
+        setTimeout(() => {document.querySelector("#beam").style.opacity = "0%";}, 50);
+        setTimeout(() => {document.querySelector("#beam").style.opacity = "45%";}, 150);
         setTimeout(() =>{
             body.classList.add("night");
             body.classList.remove("day");
